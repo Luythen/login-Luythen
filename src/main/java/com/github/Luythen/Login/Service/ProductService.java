f7ce0de2aad303395cd4caf909dea0d7a0ca8709
Luythen <andenklasson@gmail.com>
2025-12-31 15:51:47 +0100

    Product Service

---
+++ src/main/java/com/github/Luythen/Login/Service/ProductService.java
@@ -0,0 +1,26 @@
+package com.github.Luythen.Login.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.Luythen.Login.Model.ProductModel;
+import com.github.Luythen.Login.Repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public ProductModel findProductByID (UUID productID) {
+        return productRepository.findById(productID).get();
+    }
+
+    public List<ProductModel> getAllProducts () {
+        return productRepository.findAll();
+    }
+
+}
