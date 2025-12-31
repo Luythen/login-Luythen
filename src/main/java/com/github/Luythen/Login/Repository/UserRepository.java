1b87e4aaf76ca078e080b7d01feff3557ed98da4
Luythen <andenklasson@gmail.com>
2025-12-31 16:23:40 +0100

    findByEmail() method

--- src/main/java/com/github/Luythen/Login/Repository/UserRepository.java
+++ src/main/java/com/github/Luythen/Login/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.github.Luythen.Login.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.github.Luythen.Login.Model.UserModel;
 
 public interface UserRepository extends CrudRepository<UserModel, UUID> {
-    
+    Optional<UserModel> findByEmail(String email);
 }
