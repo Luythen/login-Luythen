79f90752bbb50d12d723fa0b39037e8edef6cd4e
Luythen <andenklasson@gmail.com>
2025-12-31 16:24:22 +0100

    UserService

---
+++ src/main/java/com/github/Luythen/Login/Service/UserService.java
@@ -0,0 +1,27 @@
+package com.github.Luythen.Login.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.github.Luythen.Login.Model.UserModelDto;
+import com.github.Luythen.Login.Repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+            .map(UserModelDto::new)
+            .orElseThrow(() -> new UsernameNotFoundException("Cant find"));
+    }
+
+
+
+}
